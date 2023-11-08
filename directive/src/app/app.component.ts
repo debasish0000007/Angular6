b9138b74aafd6f2a308a6da99d9d592565cf33a2
Debasish <debapadhy12@gmail.com>
2023-11-08 21:42:19 +0530

    first commit

---
+++ directive/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'directive';
+  name="shakti"
+  isAdmin: boolean =(this.name==='shakti')?true:false;
+  show: boolean = true;
+  fun()
+  {
+    alert("hellooo")
+  }
+}
